42878b5ee8129db6cafc17e875a4feeb41312a0d
mazbaul20 <mazbaul20@gmail.com>
2025-03-14 00:24:24 +0600

    class-1

---
+++ resources/js/Pages/HomePage.vue
@@ -0,0 +1,14 @@
+<script setup>
+    import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <h1>Home page</h1>
+    <Link href="/test">Test page</Link>
+    <br>
+    <button class="btn btn-primary">submit</button>
+</template>
+
+<style scoped>
+
+</style>

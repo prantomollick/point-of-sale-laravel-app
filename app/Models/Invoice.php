5ea087dc9246151efa0ebdf9c88c13d7de4e5d21
mazbaul20 <mazbaul20@gmail.com>
2025-03-21 00:46:33 +0600

    Class-3

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use App\Models\Customer;
 use Illuminate\Database\Eloquent\Model;
 
 class Invoice extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'customer_id',
+        'total',
+        'vat',
+        'payable',
+        'discount'
+    ];
+    public function customer(){
+        return $this->belongsTo(Customer::class);
+    }//end method
 }
